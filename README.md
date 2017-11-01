ccd6f2a6552683ba9221e5d48de24443d03d3202
Sebastian Richter <sebastian.max.richter@gmail.com>
2017-11-01 18:02:25 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,25 @@
+# Meteor accounts GraphQL server
+
+Exposes the functionality of the [`accounts-password`](http://docs.meteor.com/api/passwords.html) package as a GraphQL microservice. 
+
+### Quick start
+
+- Set MONGO_URL
+- `meteor`
+- 
+
+### Atmosphere packages
+
+- [`apollo`](https://docs.meteor.com/packages/apollo.html)
+- [`accounts-password`](http://docs.meteor.com/api/passwords.html)
+
+
+### NPM packages
+
+- [`apollo-client`](https://github.com/apollographql/apollo-client)
+- [`apollo-server-express`](https://github.com/apollographql/apollo-server/tree/master/packages/apollo-server-express)
+- [`body-parser`](https://github.com/expressjs/body-parser)
+- [`express`](https://github.com/expressjs/express)
+- [`graphql`](https://github.com/graphql/graphql-js)
+- [`graphql-iso-date`](https://github.com/excitement-engineer/graphql-iso-date)
+- [`graphql-tools`](https://github.com/apollographql/graphql-tools)
---
+++ server/main.js
@@ -0,0 +1,62 @@
+import { createApolloServer } from "meteor/apollo";
+import { Random } from "meteor/random";
+
+import { GraphQLDateTime } from "graphql-iso-date";
+import { makeExecutableSchema } from "graphql-tools";
+
+const typeDefs = `
+  scalar Date
+
+  type LoginResponse {
+    userId: ID!
+    loginToken: String!
+    loginTokenExpires: Date!
+  }
+
+  type Query {
+    loginWithPassword(email: String!, password: String!): LoginResponse
+  }
+`;
+
+// Inspiration for resolvers: 
+// https://github.com/davidyaha/apollo-accounts-server/blob/master/server/imports/models/user-account.js
+// https://github.com/stubailo/meteor-rest/blob/devel/packages/rest-accounts-password/rest-login.js
+
+const resolvers = {
+  Date: GraphQLDateTime,
+  Query: {
+    loginWithPassword: (obj, args, context) => {
+      return new Promise((resolve, reject) => {
+        const user = Accounts.findUserByEmail(args.email);
+        if (!user) {
+          reject(new Error("Couldn't find user."));
+        }
+        
+        const { userId, error } = Accounts._checkPassword(user, args.password);
+        if (error) {
+          reject(error);
+        }
+        
+        const token = Random.secret();
+        const when = new Date();
+        Accounts._insertLoginToken(user._id, { token, when });
+
+        // Return according to LocalStorage keys "Meteor.userId", "Meteor.loginToken", "Meteor.loginTokenExpires"
+        resolve({
+          userId: user._id,
+          loginToken: token,
+          loginTokenExpires: Accounts._tokenExpiration(when)
+        });
+      });
+    }
+  }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+createApolloServer({
+  schema,
+});
